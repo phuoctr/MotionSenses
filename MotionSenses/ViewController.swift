251f3d05eb7bd1f8e50009c2c86ad031d6ced0d0
Phuoc Trinh <trinhhuuphuoc@outlook.com>
2021-06-21 13:59:53 +0300

    Initial Commit

---
+++ MotionSenses/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MotionSenses
+//
+//  Created by Phước Trịnh on 21.6.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
